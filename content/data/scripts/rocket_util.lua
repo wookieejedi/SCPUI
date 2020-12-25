44d2bb48603e0ac7eb47ba64f580c55009020e6e
asarium <asarium@gmail.com>
2020-12-25 20:13:54 +0100

    Add HTML display support

--- content/data/scripts/rocket_util.lua
+++ content/data/scripts/rocket_util.lua
@@ -28,6 +28,61 @@ local function add_text_element(parent, document, text, color_tag, colorTable)
     parent:AppendChild(spanEl)
 end
 
+local function add_line_elements(document, paragraph, line, defaultColorTag, colorTags)
+    local searchIndex = 1
+    local colorStack = { defaultColorTag }
+
+    while true do
+        local startIdx, endIdx, colorChar, groupChar = line:find("%$(%a?)([{}]?)%s*", searchIndex)
+        if startIdx == nil then
+            break
+        end
+
+        if #colorChar == 0 and groupChar ~= "}" then
+            ba.error(string.format("Color block error in line %q", line))
+        end
+
+        -- Flush out text that was before our tag
+        local pendingText = line:sub(searchIndex, startIdx - 1)
+        add_text_element(paragraph, document, pendingText, colorStack[#colorStack], colorTags)
+
+        searchIndex = endIdx + 1
+
+        if #colorChar == 0 then
+            -- This must be the end of a color group. Remove the last color from the stack and continue
+            table.remove(colorStack)
+        else
+            table.insert(colorStack, colorChar)
+
+            if groupChar == "{" then
+                -- The start of a group so there is nothing for us to do here at the moment
+            else
+                -- We need to know if our word was terminated by white space or an explicit break so we store the whitespace
+                -- in a group and check that later
+                local rangeEndStart, rangeEndEnd, whitespace = utils.find_first_either(line, { "(%s)", "%$|" }, searchIndex)
+
+                local coloredText
+                if whitespace then
+                    -- If we broke on whitespace then we still need to include those characters in the colored range
+                    -- to ensure the spacing is correct. To do that, we build the substring until the end of the range
+                    coloredText = line:sub(endIdx + 1, rangeEndEnd)
+                else
+                    coloredText = line:sub(endIdx + 1, rangeEndStart - 1)
+                end
+                add_text_element(paragraph, document, coloredText, colorStack[#colorStack], colorTags)
+
+                table.remove(colorStack)
+
+                searchIndex = rangeEndEnd + 1
+            end
+        end
+    end
+
+    local remainingText = line:sub(searchIndex)
+    add_text_element(paragraph, document, remainingText, colorStack[#colorStack], colorTags)
+end
+
+---@param brief_text string
 function M.set_briefing_text(parent, brief_text)
     -- First, clear all the children of this element
     M.remove_children(parent)
@@ -36,6 +91,13 @@ function M.set_briefing_text(parent, brief_text)
 
     local colorTags = ui.CommandBriefing.ColorTags
     local defaultColorTag = ui.CommandBriefing.DefaultTextColorTag
+
+    local rml_mode = false
+    local escapeStart, escapeEnd = brief_text:find("^%s*!html%s*")
+    if escapeStart then
+        brief_text = brief_text:sub(escapeEnd + 1)
+        rml_mode = true
+    end
 
     local lines = utils.split(brief_text, "\n\n")
     local first = true
@@ -50,57 +112,12 @@ function M.set_briefing_text(parent, brief_text)
 
         local paragraph = document:CreateElement("p")
 
-        local searchIndex = 1
-        local colorStack = { defaultColorTag }
-
-        while true do
-            local startIdx, endIdx, colorChar, groupChar = line:find("%$(%a?)([{}]?)%s*", searchIndex)
-            if startIdx == nil then
-                break
-            end
-
-            if #colorChar == 0 and groupChar ~= "}" then
-                ba.error(string.format("Color block error in line %q", line))
-            end
-
-            -- Flush out text that was before our tag
-            local pendingText = line:sub(searchIndex, startIdx - 1)
-            add_text_element(paragraph, document, pendingText, colorStack[#colorStack], colorTags)
-
-            searchIndex = endIdx + 1
-
-            if #colorChar == 0 then
-                -- This must be the end of a color group. Remove the last color from the stack and continue
-                table.remove(colorStack)
-            else
-                table.insert(colorStack, colorChar)
-
-                if groupChar == "{" then
-                    -- The start of a group so there is nothing for us to do here at the moment
-                else
-                    -- We need to know if our word was terminated by white space or an explicit break so we store the whitespace
-                    -- in a group and check that later
-                    local rangeEndStart, rangeEndEnd, whitespace = utils.find_first_either(line, { "(%s)", "%$|" }, searchIndex)
-
-                    local coloredText
-                    if whitespace then
-                        -- If we broke on whitespace then we still need to include those characters in the colored range
-                        -- to ensure the spacing is correct. To do that, we build the substring until the end of the range
-                        coloredText = line:sub(endIdx + 1, rangeEndEnd)
-                    else
-                        coloredText = line:sub(endIdx + 1, rangeEndStart - 1)
-                    end
-                    add_text_element(paragraph, document, coloredText, colorStack[#colorStack], colorTags)
-
-                    table.remove(colorStack)
-
-                    searchIndex = rangeEndEnd + 1
-                end
-            end
+        if rml_mode then
+            -- In HTML mode, we just use the text unescaped as the inner RML
+            paragraph.inner_rml = line
+        else
+            add_line_elements(document, paragraph, line, defaultColorTag, colorTags)
         end
-
-        local remainingText = line:sub(searchIndex)
-        add_text_element(paragraph, document, remainingText, colorStack[#colorStack], colorTags)
 
         parent:AppendChild(paragraph)
     end
