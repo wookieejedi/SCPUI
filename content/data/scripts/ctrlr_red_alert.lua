5cde95e84ed60c01a289e7af4f037499d8046dfa
mjn-mixael <mjn.other@gmail.com>
2025-01-11 14:08:54 -0600

    cleanup the red alert ui

--- content/data/scripts/ctrlr_red_alert.lua
+++ content/data/scripts/ctrlr_red_alert.lua
@@ -1,98 +1,125 @@
-local async_util = require("lib_async")
-local loadoutHandler = require("lib_loadout_handler")
-local topics = require("lib_ui_topics")
+-----------------------------------
+--Controller for the Red Alert UI
+-----------------------------------
 
-local class = require("lib_class")
+local AsyncUtil = require("lib_async")
+local LoadoutHandler = require("lib_loadout_handler")
+local Topics = require("lib_ui_topics")
 
-local RedAlertController = class()
+local Class = require("lib_class")
 
+local RedAlertController = Class()
+
+--- Called by the class constructor
+--- @return nil
 function RedAlertController:init()
-	ScpuiSystem:maybePlayCutscene(MOVIE_PRE_BRIEF)
-	
-	loadoutHandler:init()
+
+	--- Check if we should play a cutscene before the briefing
+	if not ScpuiSystem.data.memory.CutscenePlayed then
+		ScpuiSystem:maybePlayCutscene(MOVIE_PRE_BRIEF)
+	end
+	ScpuiSystem.data.memory.CutscenePlayed = true
+
+	self.AlertFlashState = true --- @type boolean the state of the alert flash, true for visiable, false for hidden
+	self.AlertFlashSpeed = 0.5 --- @type number How often, in seconds, the alert should flash
+	self.Document = nil --- @type Document the RML document
+	self.CurrentVoiceHandle = nil --- @type audio_stream the current voice handle
+
+	LoadoutHandler:init()
 end
 
-local alert_el = nil
-local alert_bool = true
-
----@param document Document
+--- Called by the RML document
+--- @param document Document
 function RedAlertController:initialize(document)
 	self.Document = document
-	
+
 	---Load background choice
 	self.Document:GetElementById("main_background"):SetClass(ScpuiSystem:getBackgroundClass(), true)
-	
+
 	---Load the desired font size from the save file
 	self.Document:GetElementById("main_background"):SetClass(("base_font" .. ScpuiSystem:getFontPixelSize()), true)
 
     local alert_info = ui.RedAlert.getRedAlert()
-	
+
 	if alert_info ~= nil then
 		local text_el = self.Document:GetElementById("red_alert_text")
-		local color_text = ScpuiSystem:setBriefingText(text_el, alert_info.Text)
-	
+		ScpuiSystem:setBriefingText(text_el, alert_info.Text)
+
 		if alert_info.AudioFilename then
-			self.current_voice_handle = ad.openAudioStream(alert_info.AudioFilename, AUDIOSTREAM_VOICE)
-			self.current_voice_handle:play(ad.MasterVoiceVolume)
+			self.CurrentVoiceHandle = ad.openAudioStream(alert_info.AudioFilename, AUDIOSTREAM_VOICE)
+			self.CurrentVoiceHandle:play(ad.MasterVoiceVolume)
 		end
 	end
 
 	--Whenever we start a new mission, we reset the log ui to goals
 	ScpuiSystem.data.memory.LogSection = 1
-	
+
 	ScpuiSystem.data.memory.AlertElement = self.Document:GetElementById("incoming_transmission")
 	RedAlertController:blink()
-	
-	topics.redalert.initialize:send(self)
+
+	Topics.redalert.initialize:send(self)
 
 end
 
+--- Blink the alert element on and off
+--- @return nil
 function RedAlertController:blink()
-	
+
 	async.run(function()
-        async.await(async_util.wait_for(0.5))
-		if alert_bool then
+        async.await(AsyncUtil.wait_for(self.AlertFlashSpeed))
+		if self.AlertFlashState then
 			ScpuiSystem.data.memory.AlertElement:SetClass("hidden", true)
-			alert_bool = false
+			self.AlertFlashState = false
 			RedAlertController:blink()
 		else
 			ScpuiSystem.data.memory.AlertElement:SetClass("hidden", false)
-			alert_bool = true
+			self.AlertFlashState = true
 			RedAlertController:blink()
 		end
     end, async.OnFrameExecutor, async.context.captureGameState())
 
 end
 
+--- Called by the RML when the player presses the commit button
+--- @return nil
 function RedAlertController:commit_pressed()
-	if not topics.mission.commit:send(self) then
+	if not Topics.mission.commit:send(self) then
 		return
 	end
-	loadoutHandler:unloadAll(true)
-	topics.redalert.commit:send(self)
+	LoadoutHandler:unloadAll(true)
+	Topics.redalert.commit:send(self)
 	ba.postGameEvent(ba.GameEvents["GS_EVENT_ENTER_GAME"])
 end
 
+--- Called by the RML when the player presses the replay button
+--- @return nil
 function RedAlertController:replay_pressed()
     if ui.RedAlert.replayPreviousMission() and mn.isInCampaign() then
-		loadoutHandler:unloadAll(false)
+		LoadoutHandler:unloadAll(false)
 		ba.postGameEvent(ba.GameEvents["GS_EVENT_START_GAME"])
 	end
 end
 
+--- Called when the screen is being unloaded
+--- @return nil
 function RedAlertController:unload()
-    if self.current_voice_handle ~= nil and self.current_voice_handle:isValid() then
-        self.current_voice_handle:close(false)
+    if self.CurrentVoiceHandle ~= nil and self.CurrentVoiceHandle:isValid() then
+        self.CurrentVoiceHandle:close(false)
     end
 	ScpuiSystem.data.memory.AlertElement = nil
-	topics.redalert.unload:send(self)
+	ScpuiSystem.data.memory.CutscenePlayed = nil
+	Topics.redalert.unload:send(self)
 end
 
-function RedAlertController:global_keydown(_, event)
+--- Global keydown function handles all keypresses
+--- @param element Element The main document element
+--- @param event Event The event that was triggered
+--- @return nil
+function RedAlertController:global_keydown(element, event)
     if event.parameters.key_identifier == rocket.key_identifier.ESCAPE then
 		event:StopPropagation()
-		loadoutHandler:unloadAll(false)
-		
+		LoadoutHandler:unloadAll(false)
+
 		mn.unloadMission(true)
         ba.postGameEvent(ba.GameEvents["GS_EVENT_MAIN_MENU"])
     end
